19b59dae8346f666e7a131c25b03ae0335275d38
William Tallitsch <wtallitsch@us.imshealth.com>
2020-01-25 10:15:52 -0500

    updates

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -5,11 +5,11 @@ class Contact extends Component {
 
     if(this.props.data){
       var name = this.props.data.name;
-      var street = this.props.data.address.street;
+      // var street = this.props.data.address.street;
       var city = this.props.data.address.city;
       var state = this.props.data.address.state;
       var zip = this.props.data.address.zip;
-      var phone= this.props.data.phone;
+      // var phone= this.props.data.phone;
       var email = this.props.data.email;
       var message = this.props.data.contactmessage;
     }
@@ -81,13 +81,14 @@ class Contact extends Component {
 					   <h4>Address and Phone</h4>
 					   <p className="address">
 						   {name}<br />
-						   {street} <br />
+                     {email}<br />
+						   {/* {street} <br /> */}
 						   {city}, {state} {zip}<br />
-						   <span>{phone}</span>
+						   {/* <span>{phone}</span> */}
 					   </p>
 				   </div>
 
-               <div className="widget widget_tweets">
+               {/* <div className="widget widget_tweets">
                   <h4 className="widget-title">Latest Tweets</h4>
                   <ul id="twitter">
                      <li>
@@ -107,7 +108,8 @@ class Contact extends Component {
                         <b><a href="#">3 Days Ago</a></b>
                      </li>
                   </ul>
-		         </div>
+               </div> */}
+               
             </aside>
       </div>
    </section>
