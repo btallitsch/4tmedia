cb8d9e2bd7967253a95d4cb5a43d8768b0ed28d4
William Tallitsch <wtallitsch@us.imshealth.com>
2020-01-25 09:09:05 -0500

    Updates

---
+++ precache-manifest.de73680b1a7a84461a3e6da51d0b2761.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "beff7cf0058a36bd5678ba3acae5a651",
+    "url": "/4tmedia/index.html"
+  },
+  {
+    "revision": "0e951d9a5db1a360ee80",
+    "url": "/4tmedia/static/css/main.5facb584.chunk.css"
+  },
+  {
+    "revision": "b1b07328014df37f329c",
+    "url": "/4tmedia/static/js/2.e580c7e3.chunk.js"
+  },
+  {
+    "revision": "a1afa5f7c8dff2a316c41be2103336f5",
+    "url": "/4tmedia/static/js/2.e580c7e3.chunk.js.LICENSE"
+  },
+  {
+    "revision": "0e951d9a5db1a360ee80",
+    "url": "/4tmedia/static/js/main.6bcb6a0d.chunk.js"
+  },
+  {
+    "revision": "15d5febd493537a5a8ad",
+    "url": "/4tmedia/static/js/runtime-main.760b3e0b.js"
+  }
+]);
