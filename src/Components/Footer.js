edc9e4851cbc9cda4637e0c66d2c447b16ee6202
William Tallitsch <wtallitsch@us.imshealth.com>
2020-01-25 09:14:41 -0500

    updates

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -19,7 +19,7 @@ class Footer extends Component {
            </ul>
 
            <ul className="copyright">
-              <li>&copy; Copyright 2020 4T Media</li>
+              <li>&copy; Copyright 2020 <a title="4T Media" href="http://btallitsch.github.io/4tmedia">4T Media</a></li>
               <li>Design by <a title="Styleshout" href="http://www.styleshout.com/">Styleshout</a></li>
            </ul>
 
